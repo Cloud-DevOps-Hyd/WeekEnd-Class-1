871b1b398109d16fbc56bbbb5bec87f5dd99272d
ravindra <ravindra.devops@gmail.com>
2021-01-02 20:11:12 +0530

    sum.java V1.0

---
+++ sum.java
@@ -0,0 +1,13 @@
+class sum
+{
+	public static void main(String[] args) 
+	{
+int a,b,c;
+a=50;
+c=300;
+b=120;
+c=a+b+c;
+System.out.println ("The sum of three values are "+c);
+	}
+}
+//this is the Version Sum V1.0
