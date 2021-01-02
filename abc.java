e8d14cf8d66395b2a9078304fcf950d9f3669cd5
ravindra <ravindra.devops@gmail.com>
2021-01-02 20:48:18 +0530

    ste

---
+++ abc.java
@@ -0,0 +1,33 @@
+class abc
+{
+	public static void main(String[] args) 
+	{
+int a,b,c;
+a=50;
+c=300;
+b=120;
+c=a+b+c;
+System.out.println ("The sum of three values are "+c);
+//Substraction
+
+b=c-a;
+System.out.println("The B value is "+b);
+
+
+//division
+int d;
+d=c%b;
+System.out.println("The division of Value "+d);
+
+
+	//multiplication
+	//
+	int e;
+	e=a*a;
+	System.out.println("The C value is here"+e);
+	}
+}
+
+//this is the Version Sum V1.3
+
+
